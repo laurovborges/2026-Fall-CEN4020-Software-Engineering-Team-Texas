bc001b564d798082b9c4f0ef5166c1bd68d1a6b9
Sara Cathey <sara.cathey8@gmail.com>
2026-09-06 01:04:55 -0400

    Added code for the beginning of input including most of username and password validation.

--- InCollege.cbl
+++ InCollege.cbl
@@ -1,19 +1,94 @@
-      * Division -> Section -> Variables/Paragraphs/Statements
-       
        IDENTIFICATION DIVISION.
        PROGRAM-ID. LOGIN.
 
        ENVIRONMENT DIVISION.
-
+       Input-Output Section.
+       File-Control.
+           Select input-file
+               Assign to "InCollege-Input.txt"
+      *        Reads the file line by line
+               Organization is line sequential.
        DATA DIVISION.
       * Variables go inside this section
-       WORKING-STORAGE SECTION.
-       01 USER-NAME PIC X(20).
-
+       File Section.
+       FD input-file.
+       01 Input-Record Pic X(1000).
+       Working-Storage Section.
+      *01 line-count Pic 9(5).
+       01 username Pic X(100). 
+       01 password Pic X(12).
+       01 end-of-file Pic X Value "N".
       * This is where you do the actual logic stuff 
        PROCEDURE DIVISION.
-           MOVE "Bob" TO USER-NAME.
-           DISPLAY "Name: " USER-NAME.
-           MOVE "Jack" TO USER-NAME.
-           DISPLAY "Name: " USER-NAME.
-       STOP RUN.
+           Display "Welcome to InCollege!".
+           Display "Log In".
+           Display "Create New Account".
+           Display "Enter your choice:".
+      *     Read the file and do things
+           Open input input-file
+           Read input-file
+               Not at end
+                   If Input-Record = "Create New Account"
+                       Display "Please create a username:"
+                       Perform CREATE-USERNAME
+                       Display "Please create a password:"
+                       Perform CREATE-PASSWORD
+                       Display "Your account has been created."
+                   End-if
+
+                   If Input-Record = "Log In"
+                       Display "Please enter your username:"
+                       Perform ENTER-USERNAME
+                       Display "Please enter your password:"
+                       Perform ENTER-PASSWORD
+                       Display "You have successfully logged in."
+                   End-if
+               End-read.
+      *        IGNORE THESE COMMENTS
+      *         perform until eof
+      *         read
+      *         if user = input
+      *                end the file
+      *         endread
+      *         endperform
+           
+           Close input-file
+           STOP RUN.
+                   
+           CREATE-USERNAME.
+               Read input-file
+                   Not at end
+                       Move Input-Record to username
+               End-read.
+
+           CREATE-PASSWORD.
+               Read input-file
+                   Not at end
+                       Evaluate TRUE
+                           When Input-Record(8:1) = Space
+                               Perform CREATE-PASSWORD
+                           When Input-Record(13:1) NOT = Space
+                               Perform CREATE-PASSWORD
+      *                    When password doesn't contain:
+      *                    1 special char, 1 digit, or 1 capital letter
+      *                        Perform CREATE-PASSWORD
+                           When OTHER
+                               Move Input-Record to password
+                       End-evaluate
+               End-read.
+              
+           ENTER-USERNAME.
+               Read input-file
+                   Not at end
+      *                If username exists in the system                 
+                       Move Input-Record to username
+               End-read.
+           
+           ENTER-PASSWORD.
+               Read input-file
+                   Not at end
+      *                If password exists in the system
+                       Move Input-Record to password
+               End-read.
+
+           
